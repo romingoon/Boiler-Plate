044c68fe63bf5bad9b7e3e6752106cac9f80c3a8
romingoon <romingoon@gmail.com>
2022-10-12 17:34:26 +0900

    USER Schema 생성 완료

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://romingoon:fhals3586@boilerplate.juzatio.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => console.log('MongoDB Connected....'))
+    .catch(err => console.log(err));
+
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
